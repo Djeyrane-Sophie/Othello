ab29c238df26219c8835c7fd2dbf1abdfbbf51ec
Emmanuel <to_be_determined>
2024-11-27 16:58:03 +0100

    modif des pions

--- jeu_othello.py
+++ jeu_othello.py
@@ -5,33 +5,44 @@ from tabulate import tabulate
 #print(tabulate(matrix, tablefmt = "fancy_grid"))
 
 class Cell:
-    def __init__(self, value=0):
+
+    def __init__(self, value='.'):
         self.__value = value
+
     def __repr__(self):
         return str(self.__value)
-    @property
-    def value(self):
-        return self.__value
+
+    #getter
     def get_value(self):
         return self.__value
+    
+    #setter
     def set_value(self, new_value):
-        v = ["noir", "blanc", "vide"]
-        if new_value in v:
-            self.__value = new_value
-        pass
+        self.__value = new_value
+
 
 class Othellier:
+
     def __init__(self, row:int,col:int):
         self.matrix = [[Cell() for _ in range(row)] for _ in range(col)]
+
     def set_cell(self, value, row, col):
             self.matrix[row][col].set_value(value)
+
     def __str__(self):
         return ' '.join([' '.join([str(cell) for cell in row]) for row in self.matrix])
-        pass
-    def appercu(self):
+
+    def display(self):
         print(tabulate(self.matrix, tablefmt = "fancy_grid"))
     
 
 matrix = Othellier(8,8)
-print(matrix.appercu())
+# print(matrix.appercu())
 
+matrix.set_cell('\u26aa',4,4) #white
+matrix.set_cell('\u26aa',3,3) #white
+matrix.set_cell('\u26ab',3,4) #black
+matrix.set_cell('\u26ab',4,3) #black
+print(matrix.display())
+print('\u26aa')
+print('\u26ab')
