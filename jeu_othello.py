9960df6bc6f56f4829c76590261d44c24413b2a4
Emmanuel <emmanuel.virot@polytechnique.edu>
2024-11-28 09:36:29 +0100

    Update jeu_othello.py cleaning

--- jeu_othello.py
+++ jeu_othello.py
@@ -1,20 +1,16 @@
-import vector
+
 from tabulate import tabulate
-#matrix = [[0 for _ in range(8)] for _ in range(8)]
-#print(tabulate(matrix, tablefmt = "fancy_grid"))
+
 class Cell:
     def __init__(self, value='  '):
         self.__value = value
     def __repr__(self):
         return str(self.__value)
-    @property
-    def value(self):
-        return self.__value
     def get_value(self):
         return self.__value
     def set_value(self, new_value):
         self.__value = new_value
-        pass
+
 class Othellier:
     def __init__(self, row:int,col:int):
         self.matrix = [[Cell() for _ in range(row)] for _ in range(col)]
@@ -22,7 +18,7 @@ class Othellier:
             self.matrix[row][col].set_value(value)
     def __str__(self):
         return ' '.join([' '.join([str(cell) for cell in row]) for row in self.matrix])
-        pass
+
     def appercu(self):
         print(tabulate(self.matrix, tablefmt = "fancy_grid"))
     
