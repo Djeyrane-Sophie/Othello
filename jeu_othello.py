fc9bd68deff06a22fa01cc5ed02bc9ef9ce7d617
Sophie <sophie.taherzadeh@le-campus-numerique.fr>
2024-11-28 11:33:24 +0100

    Valid_moves is added

--- jeu_othello.py
+++ jeu_othello.py
@@ -1,39 +1,79 @@
-import vector
+
 from tabulate import tabulate
-#matrix = [[0 for _ in range(8)] for _ in range(8)]
-#print(tabulate(matrix, tablefmt = "fancy_grid"))
 class Cell:
-    def __init__(self, value='  '):
+    def __init__(self, value='.'):
         self.__value = value
     def __repr__(self):
         return str(self.__value)
-    @property
-    def value(self):
-        return self.__value
     def get_value(self):
         return self.__value
     def set_value(self, new_value):
         self.__value = new_value
-        pass
+
 class Othellier:
     def __init__(self, row:int,col:int):
         self.matrix = [[Cell() for _ in range(row)] for _ in range(col)]
     def set_cell(self, value, row, col):
             self.matrix[row][col].set_value(value)
+    def get_cell(self, row, col):
+            return self.matrix[row][col].get_value()
     def __str__(self):
-        return ' '.join([' '.join([str(cell) for cell in row]) for row in self.matrix])
-        pass
+        return ' '.join([' '.join([str(cell) for cell in row]) for row in self.matrix]) 
     def appercu(self):
         print(tabulate(self.matrix, tablefmt = "fancy_grid"))
-    
-    
-matrix = Othellier(8,8)
 
-matrix.set_cell('\u25cf',4,4)
-matrix.set_cell('\u25cf',3,3)
-matrix.set_cell('\u25cb',3,4)
-matrix.set_cell('\u25cb',4,3)
-print(matrix.appercu())
-print('\u25cf')
-print('\u25cb')
+
+
+class Rules(Othellier):
+    def valid_moves_black(self):
+        valid_places =[]
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix)):
+               if self.get_cell(i,j) == '\u26ab':
+                  direction = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,-1),(1,-1),(-1,1)]
+                  for dx, dy in direction:
+                      new_i = i + dx
+                      new_j = j + dy
+                      if 0 < new_i < len(self.matrix) and 0 < new_j < len(self.matrix):
+                          if self.get_cell(new_i, new_j) == '.':
+                              valid_places.append(self.matrix[new_i][new_j])
+                              self.set_cell('\u2705',new_i, new_j)    
+        self.appercu()     
+        return valid_places
+
+    def valid_moves_white(self):
+        valid_places =[]
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix)):
+               if self.get_cell(i,j) == '\u26aa':
+                  direction = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,-1),(1,-1),(-1,1)]
+                  for dx, dy in direction:
+                      new_i = i + dx
+                      new_j = j + dy
+                      if 0 < new_i < len(self.matrix) and 0 < new_j < len(self.matrix):
+                          if self.get_cell(new_i, new_j) == '.':
+                              valid_places.append(self.matrix[new_i][new_j])
+                              self.set_cell('\u2705',new_i, new_j)    
+        self.appercu()     
+        return valid_places
+
+matrix = Rules(8,8)
+
+
+matrix.set_cell('\u26aa',4,4)
+matrix.set_cell('\u26aa',3,3)
+matrix.set_cell('\u26ab',3,4)
+matrix.set_cell('\u26ab',4,3)
+matrix.valid_moves_black()
+
+
+
+
+
+
+
+
+print('black = \u26aa')
+print('white = \u26ab')
+print('green =\u2705')
 
