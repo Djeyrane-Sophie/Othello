cba5812647d5b7f06fe059490d1a04e018c1ec0a
Emmanuel <to_be_determined>
2024-11-28 09:55:31 +0100

    mini edits

--- jeu_othello.py
+++ jeu_othello.py
@@ -2,20 +2,22 @@
 from tabulate import tabulate
 
 class Cell:
-
+    #constructeur
     def __init__(self, value='.'):
         self.__value = value
 
     def __repr__(self):
         return str(self.__value)
-
+    
     #getter
     def get_value(self):
-        return self.__value
-    
+        return self.__value 
+      
     #setter
     def set_value(self, new_value):
         self.__value = new_value
+
+    #!method flip color to-do
 
 
 class Othellier:
@@ -29,8 +31,17 @@ class Othellier:
     def __str__(self):
         return ' '.join([' '.join([str(cell) for cell in row]) for row in self.matrix])
 
+    #method display initial and current
     def display(self):
         print(tabulate(self.matrix, tablefmt = "fancy_grid"))
+    
+    #!method input coordinates of the next move
+    #!method count points and display scores
+    #!method update cell
+
+#!Class Rule
+    #!method is_valid_move
+    #!has_valid_move (break)
     
 
 if __name__ == '__main__':
