9f09b01d696a43484bfa360a0bd23dd08514afad
Selma <selma.khounati@le-campus-numerique.fr>
2024-11-29 14:50:59 +0100

    new chages

--- jeu_othello.py
+++ jeu_othello.py
@@ -5,15 +5,13 @@ class Cell:
         self.__value = value
     def __repr__(self):
         return str(self.__value)
+    #getter
     def get_value(self):
-        return self.__value
+        return self.__value 
+    #setter
     def set_value(self, new_value):
         self.__value = new_value
-    def flip(self):  #si la coleur est noir retourner noir et si la couleur est blanc retourner blanc
-        if self.color=='\u25cf': #black
-            self.color='\u25cb' #white
-        else:
-            self.color='\u25cf'
+
 
 class Othellier:
     def __init__(self, row:int,col:int):
@@ -43,28 +41,14 @@ class Othellier:
         print('black score =', score_black)
 
 class Rules(Othellier):
+
+    #method is_valid_move for black
     def valid_moves_black(self):
         valid_places =[]
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix)):
-               if self.get_cell(i,j) == '\u26ab':
-                  direction = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,-1),(1,-1),(-1,1)]
-                  for dx, dy in direction:
-                      new_i = i + dx
-                      new_j = j + dy
-                      if 0 < new_i < len(self.matrix) and 0 < new_j < len(self.matrix):
-                          if self.get_cell(new_i, new_j) == '.':
-                              valid_places.append(self.matrix[new_i][new_j])
-                              self.set_cell('\u2705',new_i, new_j)    
-        self.display()     
-        return valid_places
-
-    def valid_moves_white(self):
-        valid_places =[]
-        for i in range(len(self.matrix)):
-            for j in range(len(self.matrix)):
                if self.get_cell(i,j) == '\u26aa':
-                  direction = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,-1),(1,-1),(-1,1)]
+                  direction = [(0,1),(1,0),(0,-1),(-1,0)]
                   for dx, dy in direction:
                       new_i = i + dx
                       new_j = j + dy
@@ -75,7 +59,24 @@ class Rules(Othellier):
         self.display()     
         return valid_places
     
-  #method input coordinates of the next move
+    #method is_valid_move for white
+    def valid_moves_white(self):
+        valid_places =[]
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix)):
+               if self.get_cell(i,j) == '\u26ab':
+                  direction = [(0,1),(1,0),(0,-1),(-1,0)]
+                  for dx, dy in direction:
+                      new_i = i + dx
+                      new_j = j + dy
+                      if 0 < new_i < len(self.matrix) and 0 < new_j < len(self.matrix):
+                          if self.get_cell(new_i, new_j) == '.':
+                              valid_places.append(self.matrix[new_i][new_j])
+                              self.set_cell('\u2705',new_i, new_j)    
+        self.display()     
+        return valid_places
+    
+    #method input coordinates of the next move
     def input_coordinates(self):
         string_input = input( 'write down the row and column without any space (example: 5B )')
         row_input = int(string_input[0])-1 #indexation entre 0 et 1
@@ -84,8 +85,7 @@ class Rules(Othellier):
         tuple_input = (row_input, col_input)
         print('Requested move:', tuple_input)
         return tuple_input
-
-
+   
     def has_valid_move(self, color):
 #  je vérifie si un joueur (noir ou blanc) a des mouvements valides.
  
@@ -106,24 +106,37 @@ class Rules(Othellier):
         return current_player_can_play, game_over 
 
 if __name__ == "__main__":
-    matrix = Rules(8, 8)
-    matrix.set_cell('\u25cf', 3, 3)
-    matrix.set_cell('\u25cb', 3, 4)
-    matrix.set_cell('\u25cb', 4, 3)
-    matrix.set_cell('\u25cf', 4, 4)
+    matrix = Rules(8,8)
+    matrix.set_cell('\u26aa',4,4) #white
+    matrix.set_cell('\u26aa',3,3) #white
+    matrix.set_cell('\u26ab',3,4) #black
+    matrix.set_cell('\u26ab',5,3) #black
 
     matrix.display()
-    current_color = '\u25cf'  # je commence avec les noirs
+    current_color = '\u26ab'  # je commence avec les noirs
     while True:
 
         can_play, game_over = matrix.has_valid_move(current_color)
         if game_over:
             print("Game Over!")
+            matrix.count_points()
             break
         elif can_play:
-            print(f"{current_color} est ton tour.Fais un mouvement.")
+            print(f"{current_color} est votre tour.")
+            row,col=matrix.input_coordinates()
             #le tour du joueur.
         else:
             print(f"{current_color} ne peut pas jouer.Passe ton tour .")
         #  je passe au joueur suivant
-        current_color = '\u25cb' if current_color == '\u25cf' else '\u25cf'
+        current_color = '\u26ab' if current_color == '\u26aa' else '\u26aa'
+
+          # print(matrix.display())
+    matrix.valid_moves_black()
+    matrix.count_points()
+    matrix.input_coordinates()
+
+
+    print('black = \u26aa')
+    print('white = \u26ab')
+    print('valid move = \u2705')
+    
